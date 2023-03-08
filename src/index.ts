f3e0007567398018edce30bce95bc78824a21a0b
OmarSaber1 <osaber218@gmail.com>
2023-03-09 01:19:55 +0200

    readonly and as const

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,20 @@ interface Person {
 type PersonOptional = Partial<Person>;
 type PersonRequired = Required<Person>;
 type PersonPick = Pick<Person, "name" | "age">;
+
+interface Cat {
+  name: string;
+  age: number;
+}
+
+type readonlycats = Readonly<Cat>;
+
+const cat: readonlycats = {
+  age: 20,
+  name: "kitty",
+};
+
+cat.age = 12;
+
+const arr = [1, 2, 3] as const;
+arr[0] = 1;
