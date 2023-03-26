1348a3e007e623c8885449260d40bd1a52293479
OmarSaber1 <osaber218@gmail.com>
2023-03-27 01:48:51 +0200

    mapped types

--- src/index.ts
+++ src/index.ts
@@ -24,3 +24,23 @@ cat.age = 12;
 
 const arr = [1, 2, 3] as const;
 arr[0] = 1;
+
+// ------------------
+
+type listners<type> = {
+  [property in keyof type as `on${Capitalize<string & property>}Change`]: (
+    newVal: type[property]
+  ) => void;
+};
+
+type dog = {
+  name: string;
+  age: number;
+};
+
+const bullDog: dog = {
+  age: 20,
+  name: "byll",
+};
+
+type listnerObject = listners<dog>;
